ca6a54ddd9c7d32f8d99c4d909a5831aa4631d8c
troncosojose22 <troncosojose22@gmail.com>
2025-11-18 12:17:23 -0500

    completed

--- index.js
+++ index.js
@@ -6,6 +6,46 @@
 
 class Song {
   //write your code here
+  static playlist = [];
+
+  #playCount = 0;
+  #rating = 0;
+
+  constructor(title, artist) {
+    this.title = title;
+    this.artist = artist;
+    
+    Song.playlist.push(this);
+  }
+  get playCount() {
+    return this.#playCount;
+  }
+
+  get rating() {
+    return this.#rating;
+  }
+
+  play() {
+    this.#playCount += 1;
+    console.log(`Now playing: ${this.title} by ${this.artist}`)
+  }
+
+  rate(stars) {
+    this.#rating = stars;
+    console.log(`You rated ${this.title} ${stars} stars`)
+  }
+
+  isPopular() {
+    return this.#playCount >= 10;
+  }
+
+  static getTotalSongs() {
+    return Song.playlist.length;
+  }
+
+  static findByTitle(title) {
+    return Song.playlist[Song.playlist.findIndex((song) => song.title == title)]
+  }
 }
 
 // Export the Song class for testing
